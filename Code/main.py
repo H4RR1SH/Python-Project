7a47f276623b07453db9a8e3378a92b572d38f3c
H4RR1SH <mohamadhharrisha@gmail.com>
2024-10-14 16:51:20 -0500

    sub class creation and formatting

--- Code/main.py
+++ Code/main.py
@@ -1,38 +1,22 @@
 class AdAnalytic:
-    
-    date = ""
-    tag = ""
-    sub_type = ""
-    type = ""
-    rating  = ""
-    cost = 0
-    predicted_ctr = 0
-     
-    def __init__(self) -> None:
-        self.unique_id = str(id(self))
-    
+    def __init__(self, date, tag, sub_type, type, rating, cost, predicted_ctr):
+        self.date = date
+        self.tag = tag
+        self.sub_type = sub_type
+        self.type = type
+        self.rating = rating
+        self.cost = cost
+        self.predicted_ctr = predicted_ctr
+        self.unique_id = id(self)
+
     def __str__(self) -> str:
-        return_str =  ','.join([self.unique_id, 
+        return_str =  ','.join([str(self.unique_id), 
                                 self.date, 
                                 self.tag, 
                                 self.sub_type, 
                                 self.type, 
-                                self.rating, 
+                                str(self.rating), 
                                 str(self.cost), 
                                 str(self.predicted_ctr)])
         
         return return_str
-    
-
-obj1 = AdAnalytic()
-
-obj1.date = "NewDate"
-obj1.tag = "NoTag"
-obj1.sub_type = "NoSubType"
-obj1.type = "NoType"
-obj1.rating  = "NoRating"
-obj1.cost = 0
-obj1.predicted_ctr = 0
-
-print(obj1.__str__())
-
